02f4922942843bba2f6db32f323f36090f4dcea8
indexpro24@gmail.com <indexpro24@gmail.com>
2022-09-18 17:28:08 +0300

    c

---
+++ database/migrations/2022_09_18_142653_create_resource_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resource', function (Blueprint $table) {
+            $table->increments('id');
+            $table->uuid('key')->default('base');
+            $table->boolean('is_active')->default(true);
+            $table->string('value', 255);
+            $table->nullableMorphs('parentable');
+            $table->timestamps();
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resource');
+    }
+};
