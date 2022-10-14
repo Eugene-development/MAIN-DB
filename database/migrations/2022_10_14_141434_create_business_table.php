1b22141f4b3f2440ced13ac2f813cec0bd735d61
indexpro24@gmail.com <indexpro24@gmail.com>
2022-10-14 17:21:55 +0300

    c

---
+++ database/migrations/2022_10_14_141434_create_business_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('business', function (Blueprint $table) {
+            $table->increments('id');
+            $table->uuid('key')->default('base');
+            $table->boolean('is_active')->default(true);
+            $table->string('value')->nullable();
+            $table->string('slug', 255)->nullable();
+            $table->nullableMorphs('parentable');
+            $table->timestamps();
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('business');
+    }
+};
