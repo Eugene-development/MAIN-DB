3e190742a9b4d6482fef01aac8e34235501c5dca
indexpro24@gmail.com <indexpro24@gmail.com>
2022-11-27 15:28:08 +0300

    с

---
+++ database/migrations/2022_11_27_104234_create_brand_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brand', function (Blueprint $table) {
+            $table->increments('id');
+            $table->uuid('key')->default('base');
+            $table->boolean('is_active')->default(true);
+            $table->string('value', 255);
+            $table->string('slug', 255);
+            $table->nullableMorphs('parentable');
+            $table->timestamps();
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brand');
+    }
+};
